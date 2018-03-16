e84443afc827f322cf47d168c21bb6bc4cd01fb7
EienArashi <32540164+EienArashi@users.noreply.github.com>
2018-03-16 09:53:35 +0100

    Add files via upload

---
+++ UX/js/script.js
@@ -0,0 +1,75 @@
+'use strict';
+// $(document).ready(function) ()
+// alert("document.ready");
+// });
+
+
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
+
+//i hope they don't mingle...
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+//function myFunction() {}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+$('li a').on('click', function (e) {
+    var targetSec = $(this).text();
+    $('html, body').animate({
+        scrollTop: $('#' + targetSec).offset().top
+    }, 2000);
+});
